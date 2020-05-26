cc237c294edf074cd7a8b8af0496759104bfc7ab
Linda Wouters <lindawouters95@gmail.com>
2020-05-26 20:50:29 +0200

    Fix conditions_inds bug that gave 0/1 accuracies

--- photobook_dataset/discriminator/helpers.py
+++ photobook_dataset/discriminator/helpers.py
@@ -155,7 +155,7 @@ def get_condition_seg_hist(conditions_inds, dataset_pred_hist_cp):
     return condition_seg_hist
 
 
-def get_pred_datasets(split='test'):
+def get_pred_datasets_orig(split='test'):
     """
     Get the dataset of predictions for the experiment 'split'.
     Using the experiment datafiles; {split}_segments.json, {split}_segments.json
@@ -198,6 +198,51 @@ def get_pred_datasets(split='test'):
     condition_seg_hist = get_condition_seg_hist(conditions_inds, dataset_pred_hist_cp)
 
     return dataset_pred_no_hist, dataset_pred_hist_cp, conditions_inds, condition_seg_hist, dataframe
+
+
+def get_pred_datasets(split='test'):
+    """
+    Get the dataset of predictions for the experiment 'split'.
+    Using the experiment datafiles; {split}_segments.json, {split}_segments.json
+    """
+    models_dict = {'History':'model_history_blind_accs_2019-02-20-14-22-23.pkl',
+                'No history': 'model_blind_accs_2019-02-17-21-18-7.pkl',
+                    'No image': 'model_history_noimg_accs_2019-03-01-14-25-34.pkl'}
+
+    # Get predictions for this experiment (split) files
+    dataset_pred_no_hist = get_predictions(model_name='No history', models_dict=models_dict, split=split)
+
+    # History dataset takes about 20 minutes to run
+    dataset_pred_hist = get_predictions(model_name='History', models_dict=models_dict, split=split)
+
+    # Load in the segment ids that tell us which segment belongs to which index in the history dataset
+
+    # Segment_ids_file segment_ids_test.json is created when predicting dataset_pred_hist
+    segment_ids = get_seg_ids(segment_ids_file='segment_ids_test.json')
+
+    # Get inverted dict {seg_id:dataset_ind}
+    inv_list = create_inv_list(segment_ids)
+
+    # Create new history dataset with the segments in the same order as the no-history dataset
+    dataset_pred_hist_cp = reorder_datast(dataset_pred_hist, inv_list)
+
+    # Add the chain indices and the round in those chains per segement
+    chain_test_set = ChainDataset(
+        data_dir='data/',
+        segment_file='segments.json',
+        chain_file='test_chains.json',
+        vectors_file='vectors.json',
+        split=split
+    )
+    # Get dataset with for each
+    dataset_pred_no_hist, dataset_pred_hist_cp = add_chains_rounds(dataset_pred_no_hist, dataset_pred_hist_cp, chain_test_set)
+
+    # dataframe = get_pred_dataframe(dataset_pred_no_hist, dataset_pred_hist_cp)
+    # conditions_inds = get_conditions_inds(dataframe)
+
+    # condition_seg_hist = get_condition_seg_hist(conditions_inds, dataset_pred_hist_cp)
+
+    return dataset_pred_no_hist, dataset_pred_hist_cp
 
 
 def get_accuracies(conditions_inds, dataframe):
